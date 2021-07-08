cb8e109cad71b9cb19546a5c5cfa7cc36aa5db4d
chrisdeleonn <cdizzel@ymail.com>
2021-07-08 15:20:20 -0400

    added .env, connected to mongo, updated .gitignore, tested ok

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+require("dotenv/config");
 
 mongoose
-.connect('mongodb+srv://root:root123@cluster0.up5z2.mongodb.net/test', { useNewUrlParser: true})
-.then(() => console.log('we are connected Houston...'))
-.catch(err => console.log('you have an error buddy, fix it...', err))
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("we are connected Houston..."))
+  .catch((err) => console.log("you have an error buddy, fix it...", err));
 
+const movieSchema = mongoose.Schema({
+  title: String,
+  genre: [String],
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+function getAllMovies() {
+  Movie.find()
+    .then((allMovies) =>
+      console.log("here are all the movies as requested", allMovies)
+    )
+    .catch((err) =>
+      console.log("you broke something, assemble the monkeys to fix", err)
+    );
+}
+
+getAllMovies();
